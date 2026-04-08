090ace2fa26e148eea0bec23068efc938f1a092b
Ahmad Bahrudin Ilham <ahmadbhrdilham@gmail.com>
2026-04-08 16:13:21 +0700

    push

--- internal/config/messages.go
+++ internal/config/messages.go
@@ -5,27 +5,27 @@ const (
 	MsgOnlyGroup      = "Perintah ini hanya bisa digunakan di dalam grup."
 	MsgOnlyAdmin      = "Perintah ini hanya untuk admin grup."
 	MsgMenu           = `
-• .s / .sticker _(Membuat sticker)_
-• .ts <text> _(Sticker teks dinamis)_
-• .brat <text> _(Brat sticker)_
-• .toimg _(Sticker jadi Gambar / Buka View Once)_
-• .dl <link> _(Download Video)_
-• .mp3 <link> _(Download Audio)_
+• .s / .sticker 
+• .ts <text> 
+• .brat <text> 
+• .toimg 
+• .dl <link> 
+• .mp3 <link> 
 
-• .tagall _(Mention seluruh member)_
-• .autotag _(On/Off Radar Link TikTok)_
-• .warn <@tag> _(Beri peringatan SP)_
-• .resetwarn <@tag> _(Hapus SP member)_
-• .kick <@tag> _(Keluarkan member)_
+• .tagall
+• .autotag
+• .warn 
+• .resetwarn 
+• .kick 
 
-• .bansticker _(Reply sticker porn/spam)_
-• .unbansticker <alias> _(Cabut pemblokiran)_
-• .liststicker _(Daftar sticker terblokir)_
-• .banuser <@tag> _(Blokir hak kirim sticker)_
-• .unbanuser <@tag> _(Cabut blokir user)_
-• .listuser _(Daftar pelaku terblokir)_
+• .bansticker 
+• .unbansticker 
+• .liststicker 
+• .banuser
+• .unbanuser 
+• .listuser 
 
-• .read _(Bongkar chat terhapus dari reply)_
-• .stat _(Cek Server Bot & Uptime)_
-• .menu _(Menampilkan pesan ini)_`
+• .read 
+• .stat 
+• .menu `
 )
